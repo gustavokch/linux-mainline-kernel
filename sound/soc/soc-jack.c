8e849374b7ab7d70715780691d7d1ac048286c85
Kamil Trzciński <ayufan@ayufan.eu>
2025-03-14 17:30:30 +0100

    Revert part of pbp-support sound/soc/soc-jack.c 0dbe662dd54783d1155e3302899dc3dc75ef5033

--- sound/soc/soc-jack.c
+++ sound/soc/soc-jack.c
@@ -344,24 +344,9 @@ int snd_soc_jack_add_gpios(struct snd_soc_jack *jack, int count,
 				goto undo;
 			}
 		} else {
-			int flags = GPIOF_IN;
-			if (gpios[i].invert)
-				flags |= GPIOF_ACTIVE_LOW;
-			/* legacy GPIO number */
-			if (!gpio_is_valid(gpios[i].gpio)) {
-				dev_err(jack->card->dev,
-					"ASoC: Invalid gpio %d\n",
-					gpios[i].gpio);
-				ret = -EINVAL;
-				goto undo;
-			}
-
-			ret = gpio_request_one(gpios[i].gpio, flags,
-					       gpios[i].name);
-			if (ret)
-				goto undo;
-
-			gpios[i].desc = gpio_to_desc(gpios[i].gpio);
+			dev_err(jack->card->dev, "ASoC: Invalid gpio at index %d\n", i);
+		        ret = -EINVAL;
+		        goto undo;
 		}
 got_gpio:
 		INIT_DELAYED_WORK(&gpios[i].work, gpio_work);
